d684c495e4ab131d31c816b857f3e1d5c0f6506d
Kerim <sarikerim@hotmail.de>
2022-12-20 21:03:33 +0100

    Added solution for day 6

--- AdventOfCode/Day6.cs
+++ AdventOfCode/Day6.cs
@@ -14,10 +14,40 @@ namespace AdventOfCode
 
             var input = File.ReadAllLines("./Input/Day6.txt");
 
+            DetectStartOfMarker("Start-Of-Packet", 4, input[0]);
+            DetectStartOfMarker("Start-Of-Message", 14, input[0]);
 
             Console.WriteLine();
             Console.WriteLine("============== End of Code for Day6 ===============");
             Console.WriteLine();
+        }
+
+        private static void DetectStartOfMarker(string markerType, int markerLength, string input)
+        {
+            var markerDetected = true;
+            var index = 0;
+
+            while (markerDetected)
+            {
+                var distinctCount = input.Skip(index)
+                                         .Take(markerLength)
+                                         .Distinct()
+                                         .Count();
+
+                if (distinctCount == markerLength)
+                {
+                    markerDetected = false;
+                    break;
+                }
+
+                index++;
+            }
+
+            var marker = input.Substring(index, markerLength);
+
+            Console.WriteLine($"{markerType}");
+            Console.WriteLine($"Full marker: {marker}");
+            Console.WriteLine($"Marker detected at index: {index + markerLength}");
         }
     }
 }
